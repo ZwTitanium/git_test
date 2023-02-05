4a984b46471a609575b186b3585fb389a52ad44a
zhaiwei <1340446738@qq.com>
2023-02-05 17:03:14 +0800

    verson1.0

---
+++ src/main/java/com/zw/GitTest.java
@@ -0,0 +1,11 @@
+package com.zw;
+
+/**
+ * @author shenhai
+ * @date:2023/2/5
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
