b712bdd9f9cc2bbc0af4da7ba5e6dfe93002434f
zhaiwei <1340446738@qq.com>
2023-02-05 17:07:06 +0800

    verson3.0

--- src/main/java/com/zw/GitTest.java
+++ src/main/java/com/zw/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("第二个版本");
+        System.out.println("第三个版本");
     }
 }
