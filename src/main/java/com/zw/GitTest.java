f2cde8eacaf3e76756c7d0f02e0bf30ad48f7d16
zhaiwei <1340446738@qq.com>
2023-02-05 17:26:11 +0800

    verson4.0

--- src/main/java/com/zw/GitTest.java
+++ src/main/java/com/zw/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("第二个版本");
         System.out.println("第三个版本");
         System.out.println("来自hot-fix的第一个版本");
+        System.out.println("第四个版本");
     }
 }
