38b322baabe2092ea906c49219cad67780317fa5
zhaiwei <1340446738@qq.com>
2023-02-05 20:00:59 +0800

    verson push test

--- src/main/java/com/zw/GitTest.java
+++ src/main/java/com/zw/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("第四个版本");
         System.out.println("来自hot-fix的第二个版本");
         System.out.println("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");
+        System.out.println("push test");
     }
 }
